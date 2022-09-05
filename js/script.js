b927d06ea1fb256051d1002e45b659840ea066df
hemhem <kimhp1216@gmail.com>
2022-09-05 12:10:53 +0900

    stx건설 클론 코딩

--- js/script.js
+++ js/script.js
@@ -1,12 +1,42 @@
+$(document).ready(function(){
+  $('html').css('overflow','hidden');
+  let modalWrap = $('.modal-wrap');
+  let modalClose = $('.modal-close');
+  modalClose.click(function(){
+    modalWrap.stop().fadeOut(200);
+    $('html').css('overflow','auto');
+  });
+  let modalMain = $('.modal-main');
+  modalMain.click(function(event){
+    event.stopPropagation();
+  });
+  modalWrap.click(function(){
+    modalWrap.stop().fadeOut(200);
+    $('html').css('overflow','auto');
+  });
+});
+
 $(document).ready(function () {
-  // .header를 저장한다.
+
   let header = $('.header');
   let gnb = $('.gnb');
 
+  // 서브메뉴 높이값
+  let gnbMaxHeight = gnb.outerHeight();
+  let gnbMinHeight = header.outerHeight();
+  header.css('height', gnbMinHeight);
+  gnb.mouseenter(function () {
+    header.css('height', gnbMaxHeight);
+  });
+  gnb.mouseleave(function () {
+    header.css('height', gnbMinHeight);
+  });
+
+
+  // 서브메뉴열기
   gnb.mouseenter(function () {
     header.addClass('header-open');
   });
-
   gnb.mouseleave(function () {
     header.removeClass('header-open');
   });
@@ -24,17 +54,27 @@ $(document).ready(function () {
 
   // 컨텐츠 이동 버튼
   let visualBt = $('.visual-bt');
-  visualBt.click(function () {});
+  let partY = $('.part').offset().top;
+
+  visualBt.click(function () {
+    $('html').animate({
+      scrollTop: partY
+    });
+  });
+
 
   // part 배경 배치
   let partListLi = $('.part-list li');
   let partListW = 1200 / partListLi.length;
-  $.each(partListLi, function(index){
+  $.each(partListLi, function (index) {
     let tgX = partListW * index;
     tgX = -1 * tgX;
-    
+
     $(this).css('background-position-x', tgX);
   });
+  
+  let first = $('.part-list-li').eq(0);
+  let last = $('.part-list-li').eq(4);
 
 });
 
